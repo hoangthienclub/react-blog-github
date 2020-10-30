b4ed5013597da4a2caa33711688a04280862de62
Tran Thien <thien.tran@saigonvalley.com>
2020-10-30 16:56:57 +0700

    first init

---
+++ src/config.js
@@ -0,0 +1,32 @@
+export const config = {
+  // Your Github Converted Token To Know How To Get Your Token Look at Readme.md
+  githubConvertedToken: "N2M5ZGI3ODkzOTJkZmRhMWFkOGU1N2VlZTY5YzFmMWZlOTc3MTgwYg==",
+
+  // Your Github UserName
+  githubUserName: "hoangthienclub",
+
+  // Your Github Repo Name Where You Have your issues as Blog
+  githubRepo: "react-blog-github",
+
+  // Set it to true if you have a Github app to add to this project
+  // and fill the client ID & secret
+  enableOAuth: true,
+  OAuthClientID: 'Iv1.497541269f9cb137',
+  OAuthSecret: 'd24095d57bed94e938896243c325f26b563362fb',
+
+  // Your Personal Blog Title
+  title : " Thiện Trần Blog" ,
+
+  // Your Personal Blog Subtitle
+  subtitle : "Software Developer",
+
+  // Header customization
+  header: {
+    backgroundColor: '#f1f6f8', // can be a CSS gradient
+    backgroundColorDark: '#4C566A',
+    titleColor: '#ff5252',
+    titleColorDark: '#16a085',
+    subtitleColor: '#37474f',
+    subtitleColorDark: '#D8DEE9',
+  },
+};
