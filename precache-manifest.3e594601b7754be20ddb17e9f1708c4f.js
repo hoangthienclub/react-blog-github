99f0274b21214f5111457d7716f6f1236dcb27e0
Tran Thien <thien.tran@saigonvalley.com>
2020-10-30 17:18:29 +0700

    Updates

---
+++ precache-manifest.3e594601b7754be20ddb17e9f1708c4f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d45f4d4bab7cfb5f2087746386e5d86e",
+    "url": "/react-blog-github/index.html"
+  },
+  {
+    "revision": "626f41552549f590e744",
+    "url": "/react-blog-github/static/css/main.cfa08aa7.chunk.css"
+  },
+  {
+    "revision": "cf352fcb46f8dc475556",
+    "url": "/react-blog-github/static/js/2.77de8f68.chunk.js"
+  },
+  {
+    "revision": "189e23ef3562d267680aa9086e84695d",
+    "url": "/react-blog-github/static/js/2.77de8f68.chunk.js.LICENSE"
+  },
+  {
+    "revision": "626f41552549f590e744",
+    "url": "/react-blog-github/static/js/main.5558274d.chunk.js"
+  },
+  {
+    "revision": "8216af6db86cb58257df",
+    "url": "/react-blog-github/static/js/runtime-main.b7e97252.js"
+  }
+]);
